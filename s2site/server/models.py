9267658c5694d55a3a49a41dd4f6580bec8a19d6
Michael Kaeser <me@michaelk.ch>
2009-08-16 00:00:08 +0200

    Added list view for active servers

--- s2site/server/models.py
+++ s2site/server/models.py
@@ -1,6 +1,13 @@
+from datetime import datetime, timedelta
 from django.db import models
 
+class ActiveServerManager(models.Manager):
+    def get_query_set(self):
+        return super(ActiveServerManager, self).get_query_set().filter(
+        	updated__gte=datetime.now() - timedelta(minutes=10))
+
 class Server(models.Model):
+	# Fields
     port = models.IntegerField()
     ip = models.CharField(max_length=15)
     max_conn = models.IntegerField()
@@ -13,5 +20,9 @@ class Server(models.Model):
     login = models.CharField(max_length=50)
     updated = models.DateTimeField(auto_now=True, auto_now_add=True)
 
+	# Managers
+    objects = models.Manager()
+    active = ActiveServerManager()    
+    
     def __unicode__(self):
         return "%s:%s" % (self.ip, self.port)
--- s2site/server/views.py
+++ s2site/server/views.py
@@ -1,5 +1,8 @@
 from lib.decorators import render_to
 
+from s2site.server.models import Server
+
 @render_to('server/list.html')
 def list(request):
+	servers = Server.active.all()
 	return locals()

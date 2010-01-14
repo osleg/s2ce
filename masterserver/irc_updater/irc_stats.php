6fb4617c8b3a1c57e97ed70d0dbfbc6a822db057
Michael Käser <michaelk@michael-kasers-macbook-pro.local>
2010-01-14 21:33:15 +0100

    Added comm fields

--- masterserver/irc_updater/irc_stats.php
+++ masterserver/irc_updater/irc_stats.php
@@ -4,30 +4,42 @@ include("../common/lib.php");
 
 /* Valid stats fields */
 $fields = array(
-	'team',
-	'karma',
-	'exp',
-	'kills',
-	'deaths',
-	'assists',
-	'souls',
-	'razed',
-	'pdmg',
-	'bdmg',
-	'npc',
-	'hp_healed',
-	'res',
-	'gold',
-	'hp_repaired',
-	'secs',
-	'end_status',
-	'sf',
-	'auto_win',
-	'rec_stats',
-	'malphas',
-	'revenant',
-	'devourer',
-	'id');
+	'action' => array(
+		'exp',
+		'kills',
+		'deaths',
+		'assists',
+		'souls',
+		'razed',
+		'pdmg',
+		'bdmg',
+		'npc',
+		'hp_healed',
+		'res',
+		'gold',
+		'hp_repaired',
+		'secs',
+		'end_status',
+		'sf',
+		'ip'
+	),
+	'commander' => array(
+		'builds',
+		'exp',
+		'gold',
+		'razed',
+		'hp_healed',
+		'pdmg',
+		'kills',
+		'debuffs',
+		'buffs',
+		'orders',
+		'secs',
+		'end_status',
+		'sf',
+		'ip'
+	)
+);
 
 /* Dispatch request into handle function */
 dispatch_request(array("end_game"));
@@ -37,7 +49,6 @@ function handle_end_game()
 {
 	global $fields;
 	
-	$player_stats = post_input("player_stats");
 	$match_id = intval(post_input("match_id"));
 	$map = post_input("map");
 	$winner = intval(post_input("winner"));
@@ -49,47 +60,51 @@ function handle_end_game()
 		INSERT INTO
 			matches
 		SET
-			id = {$match_id},
-			map = '{$map}',
-			duration = '{$duration}',
-			winner = {$winner},
-			raw = '{$raw}'";
+			`id` = {$match_id},
+			`map` = '{$map}',
+			`duration` = '{$duration}',
+			`winner` = {$winner},
+			`raw` = '{$raw}'";
 	db_query($query);
 	
 	/* Insert teams */
-	$stats_teams = unserialize(post_input("teams"));
-	$teams = array();
-	foreach ($stats_teams as $id => $team) {
+	$teams = post_serialized("team");
+	$team_ids = array();
+	foreach ($teams as $index => $team) {
 		$query = "
 			INSERT INTO
 				teams
 			SET
-				match = {$match_id},
-				race = '{$team['race']}',
-				avg_sf = {$team['avg_sf']},
-				commander = {$team['commander']}";
+				`match` = {$match_id},
+				`race` = '{$team['race']}',
+				`avg_sf` = {$team['avg_sf']},
+				`commander` = {$team['commander']}";
 		db_query($query);
-		$teams[$id] = mysql_insert_id();
+		$team_ids[$index] = mysql_insert_id();
 	}
 
 	/* Insert player stats */
-	$stats = unserialize(post_input("player_stats"));	
-	foreach ($stats as $player) {
+	$player_stats = post_serialized("player_stats");
+	foreach ($player_stats as $player) {
 		$user_id = $player['account_id'];
+		$team_id = $team_ids[$player['team']];
 		
 		$query = "
 			INSERT INTO
 				actionplayers
 			SET
-				user = {$user_id},
-				match = {$match_id}";
+				`user` = {$user_id},
+				`match` = {$match_id},
+				`team` = {$team_id}";
 		
 		// stats fields
 		foreach ($fields as $field) {
-			$query .= ", {$field} ≠ {$player[$field]}";
+			$query .= ", `{$field}` = '{$player[$field]}'";
 		}
 		
-		$result = db_query($query);
+		var_dump($query);
+		
+		db_query($query);
 	}
 	
 	return array();

3135433335cb0ab0bfffa17a75f50c8352a17106
Michael Käser <michaelk@michael-kasers-macbook-pro.local>
2010-01-16 19:54:15 +0100

    Added SQL schema

---
+++ masterserver/schema.sql
@@ -0,0 +1,158 @@
+-- MySQL dump 10.13  Distrib 5.1.38, for apple-darwin9.5.0 (i386)
+--
+-- Host: localhost    Database: s2ce
+-- ------------------------------------------------------
+-- Server version	5.1.38
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `actionplayers`
+--
+
+DROP TABLE IF EXISTS `actionplayers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `actionplayers` (
+  `user` int(11) NOT NULL,
+  `match` int(11) NOT NULL,
+  `team` int(11) NOT NULL,
+  `exp` int(11) NOT NULL,
+  `kills` int(11) NOT NULL,
+  `deaths` int(11) NOT NULL,
+  `assists` int(11) NOT NULL,
+  `souls` int(11) NOT NULL,
+  `razed` int(11) NOT NULL,
+  `pdmg` int(11) NOT NULL,
+  `bdmg` int(11) NOT NULL,
+  `npc` int(11) NOT NULL,
+  `hp_healed` int(11) NOT NULL,
+  `res` int(11) NOT NULL,
+  `gold` int(11) NOT NULL,
+  `hp_repaired` int(11) NOT NULL,
+  `secs` int(11) NOT NULL,
+  `end_status` int(11) NOT NULL,
+  `sf` int(11) NOT NULL,
+  `ip` varchar(50) NOT NULL,
+  PRIMARY KEY (`user`,`match`,`team`),
+  KEY `user` (`user`),
+  KEY `match` (`match`),
+  KEY `match_2` (`match`,`team`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `commanders`
+--
+
+DROP TABLE IF EXISTS `commanders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `commanders` (
+  `user` int(11) NOT NULL DEFAULT '0',
+  `match` int(11) NOT NULL DEFAULT '0',
+  `team` int(11) NOT NULL DEFAULT '0',
+  `builds` int(11) NOT NULL,
+  `exp` int(11) NOT NULL,
+  `gold` int(11) NOT NULL,
+  `razed` int(11) NOT NULL,
+  `hp_healed` int(11) NOT NULL,
+  `pdmg` int(11) NOT NULL,
+  `kills` int(11) NOT NULL,
+  `debuffs` int(11) NOT NULL,
+  `buffs` int(11) NOT NULL,
+  `orders` int(11) NOT NULL,
+  `secs` int(11) NOT NULL,
+  `end_status` int(11) NOT NULL,
+  `sf` int(11) NOT NULL,
+  `ip` varchar(50) NOT NULL,
+  PRIMARY KEY (`user`,`match`,`team`),
+  KEY `user` (`user`),
+  KEY `match` (`match`),
+  KEY `match_2` (`match`,`team`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `matches`
+--
+
+DROP TABLE IF EXISTS `matches`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `matches` (
+  `id` int(11) NOT NULL DEFAULT '0',
+  `server` int(11) DEFAULT NULL,
+  `winner` int(11) DEFAULT NULL,
+  `duration` int(11) DEFAULT NULL,
+  `map` varchar(50) DEFAULT NULL,
+  `raw` text,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `servers`
+--
+
+DROP TABLE IF EXISTS `servers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `servers` (
+  `id` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `teams`
+--
+
+DROP TABLE IF EXISTS `teams`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `teams` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `match` int(11) NOT NULL,
+  `race` varchar(15) NOT NULL,
+  `avg_sf` int(11) NOT NULL,
+  `commander` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `matchrace` (`match`,`race`),
+  KEY `match` (`match`)
+) ENGINE=MyISAM AUTO_INCREMENT=57 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) DEFAULT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2010-01-16 19:54:08
